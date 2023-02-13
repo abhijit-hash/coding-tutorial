388a89194e116903fe2bb2bbcbfe41b6d756d926
Abhijit Pal <subujpaul2003@gmail.com>
2023-02-13 15:29:59 +0530

    my third commit

---
+++ leetcode-3.cpp
@@ -0,0 +1,22 @@
+    #include<bits/stdc++.h>
+    using namespace std;
+    int countEven(int num) {
+        long long int c=0;
+        for(int i=1;i<=num;i++){
+            long long int j=i,ans=0;
+            while(j>0){
+                ans+=j%10;
+                j=j/10;
+            }
+            if(ans%2==0){
+                c++;
+            }
+        }
+        return c;
+    }
+    int main(){
+        int num;
+        cin>>num;
+        return countEven(num);
+
+    }
